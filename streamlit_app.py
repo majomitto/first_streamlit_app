3db849560d4838c7f67f192eda059eb6d09bcd82
majomitto <106969546+majomitto@users.noreply.github.com>
2022-09-08 11:22:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import requests
 import pandas
 import snowflake.connector
 from urllib.error import URLError
-
+#work in testing branch
 streamlit.title('New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
