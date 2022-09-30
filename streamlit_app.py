965c533e97531d32faa3ac6fe5d7cfc75625be3a
majomitto <majomitto@gmail.com>
2022-09-30 09:32:13 +0200

    test modification vie tortoise

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import requests
 import pandas
 import snowflake.connector
 from urllib.error import URLError
-#work in testing branch
+#work in testing branch checkout
 streamlit.title('New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
